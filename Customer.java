f104cbf8d3c1a0fbe6a45d7fe57ed373de46261c
Rushitapatell <rushita2849@outlook.com>
2025-02-26 17:51:55 -0500

    Create Customer.java

---
+++ Customer.java
@@ -0,0 +1,75 @@
+/** Project:Database Assignment
+ * Course: IST 242
+ * Author:Rushita Patel
+ * Date Developed:02/24/25
+ * Last Date Changed:02/26/25
+
+ */
+public class Customer {
+    private int id;
+    private String name;
+    private String email;
+    private String phone;
+    private String address;
+
+    // Constructor
+    public Customer(int id, String name, String email, String phone, String address) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.phone = phone;
+        this.address = address;
+    }
+
+    // Getters
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    // Setters
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    // Behavior: Display Customer Info
+    public void displayCustomerInfo() {
+        System.out.println("Customer ID: " + id);
+        System.out.println("Name: " + name);
+        System.out.println("Email: " + email);
+        System.out.println("Phone: " + phone);
+        System.out.println("Address: " + address);
+    }
+}
+
